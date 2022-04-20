cf9de658ec32d8bbba50443864c07dc953d5965c
Nils <Nils.Jadefalke@gmail.com>
2022-04-20 15:42:16 +0200

    main crawler functionality

--- src/main/java/io/github/jadefalke2/WebCrawlerApplication.java
+++ src/main/java/io/github/jadefalke2/WebCrawlerApplication.java
@@ -1,13 +1,35 @@
 package io.github.jadefalke2;
 
+import io.github.jadefalke2.model.Crawler;
 import picocli.CommandLine;
 
-public class WebCrawlerApplication {
+import static picocli.CommandLine.*;
+
+public class WebCrawlerApplication implements Runnable {
+
+	@Parameters(description = "The url to crawl")
+	private String url;
+
+	@Option(names = {"--depth", "-d"}, description = "The depth to crawl for", defaultValue = "3")
+	private int maxDepth;
+
+	
+	private final Crawler crawler;
+
+	public WebCrawlerApplication () {
+		crawler = new Crawler();
+	}
+
+	@Override
+	public void run () {
+		var res = crawler.crawl(url, maxDepth);
+		System.out.println(res);
+	}
+
 
 	public static void main (String[] args) {
 		var cmd = new CommandLine(new WebCrawlerApplication());
 		int exitCode = cmd.execute(args);
 		System.exit(exitCode);
 	}
-
 }
--- src/main/java/io/github/jadefalke2/model/Crawler.java
+++ src/main/java/io/github/jadefalke2/model/Crawler.java
@@ -1,2 +1,49 @@
-package io.github.jadefalke2.model;public class Crawler {
+package io.github.jadefalke2.model;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Crawler {
+
+	public Set<String> crawl (String startingURL, int depth) {
+		return crawl(startingURL, depth, 0);
+	}
+
+	private Set<String> crawl (String startingURL, int maxDepth, int depth) {
+		var urls = getURLs(startingURL);
+		if (depth < maxDepth) {
+			return urls.stream()
+			           .map(url -> and(crawl(url, maxDepth, depth + 1), url))
+			           .flatMap(Set::stream)
+			           .collect(Collectors.toSet());
+		}
+		return Collections.emptySet();
+	}
+
+	private <T> Set<T> and (Set<T> set, T elem) {
+		var nSet = new HashSet<>(set);
+		nSet.add(elem);
+		return nSet;
+	}
+
+	private Set<String> getURLs (String url) {
+		try {
+			return Jsoup.connect(url)
+			            .get()
+			            .select("a[href]")
+			            .stream()
+			            .map(l -> l.attr("abs:href"))
+						.filter(l -> !l.isEmpty())
+			            .collect(Collectors.toSet());
+		} catch (IOException ignored) {
+			// ignore exception
+			return Collections.emptySet();
+		}
+	}
 }
