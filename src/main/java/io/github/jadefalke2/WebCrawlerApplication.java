f930b416d6539149ec4fcfdbeeb6867fad6c544c
Nils <Nils.Jadefalke@gmail.com>
2022-04-20 14:31:06 +0200

    bootstrap picocli

--- src/main/java/io/github/jadefalke2/WebCrawlerApplication.java
+++ src/main/java/io/github/jadefalke2/WebCrawlerApplication.java
@@ -1,9 +1,13 @@
 package io.github.jadefalke2;
 
+import picocli.CommandLine;
+
 public class WebCrawlerApplication {
 
 	public static void main (String[] args) {
-
+		var cmd = new CommandLine(new WebCrawlerApplication());
+		int exitCode = cmd.execute(args);
+		System.exit(exitCode);
 	}
-	
+
 }
