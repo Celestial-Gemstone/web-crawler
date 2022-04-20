267dfd0404d9d759b2bc7acd34cfe9c8c7556010
Nils <Nils.Jadefalke@gmail.com>
2022-04-20 16:44:20 +0200

    revert required parameter addition

--- src/main/java/io/github/jadefalke2/WebCrawlerApplication.java
+++ src/main/java/io/github/jadefalke2/WebCrawlerApplication.java
@@ -7,7 +7,7 @@ import static picocli.CommandLine.*;
 
 public class WebCrawlerApplication implements Runnable {
 
-	@Parameters(description = "The url to crawl", required = true)
+	@Parameters(description = "The url to crawl")
 	private String url;
 
 	@Option(names = {"--depth", "-d"}, description = "The depth to crawl for", defaultValue = "3")
